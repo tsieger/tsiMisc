6298c128f2024e5d1edf6e16b43a300905a3f782
Tomas Sieger <tsieger@server.fake>
2016-02-29 09:05:36 +0100

    making more sense for factors

--- R/countFactorLevels.R
+++ R/countFactorLevels.R
@@ -1,9 +1,15 @@
 countFactorLevels<-structure(
 function # Count the number of factor levels.
-##<<details
+##description<<
+## 'countFactorLevels' summarizes a data frame, a list, or a factor by
+## computing the number of declared and used factor levels in it (in
+## case of factors) or its constituents (in case of data frames and
+## lists).
 (x ##<< a data frame, a list, or a factor
 ) {
-  if (!is.list(x)) {
+  if (is.factor(x)) {
+    x<-as.data.frame(x)
+  } else if (!is.list(x)) {
     x<-as.list(x)
   }
   res<-sapply(x,function(x)ifelse (is.factor(x), length(levels(x)),NA))
@@ -20,5 +26,7 @@ function # Count the number of factor levels.
     # omitting the first row results in some levels of 'Cars$Model' and
     # 'Cars93$Make' unused (and reported in the second row)
     countFactorLevels(Cars93[-1L, ])
+
+    countFactorLevels(Cars93$Model[-1L])
     }
 })
