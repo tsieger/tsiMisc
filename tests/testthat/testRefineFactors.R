fa38b7be6c593445902f33836dd27037974e4150
Tomas Sieger <tsieger@users.noreply.github.com>
2016-03-02 11:16:12 +0100

    renamed

--- tests/testthat/testRefineFactors.R
+++ tests/testthat/testRefineFactors.R
@@ -3,8 +3,8 @@ x.train<-x[1:3,]
 x.test<-x[4:6,]
 
 res<-refineFactors(x.train,x.test)
-expect_that(nrow(res$x1), equals(0))
-expect_that(nrow(res$x2), equals(0))
+expect_that(ncol(res$x1), equals(0))
+expect_that(ncol(res$x2), equals(0))
 
 res<-refineFactors(x.train, x.test, dropSingular = FALSE)
 expect_that(nrow(res$x1), equals(1))
