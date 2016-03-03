4d275b5a6e66ae41819d511c12180b06b03a1cb1
Tomas Sieger <tsieger@users.noreply.github.com>
2016-03-03 19:42:13 +0100

    centering, scaling and debug added

--- R/spca.R
+++ R/spca.R
@@ -10,10 +10,31 @@ function # Supervised PCA.
 ## Pattern Recognition, Vol. 44, No. 7. (29 July 2011), pp. 1357-1371,
 ## doi:10.1016/j.patcog.2010.12.015.
 (x, ##<< a data matrix (features in columns, samples in rows)
-xLabels ##<< classification of x (logical or factor)
+xLabels, ##<< classification of x (logical, numeric, or a factor)
+center = TRUE, ##<< a logical value indicating whether to center the
+## data. This is advisable.
+scale = FALSE, ##<< a logical value indicating whether to scale the
+## data to have unit variance.
+retx = FALSE, ##<< a logical value indicating whether to return the
+## rotated version of 'x'
+debug = FALSE ##<< if TRUE, debugs will be printed. If numeric of value
+## greater than 1, verbose debugs will be produced.
 ) {
   if (!is.matrix(x)) x<-as.matrix(x)
   if (!is.factor(xLabels)) xLabels<-as.factor(xLabels)
+
+  if (is.logical(center) && center) {
+    x<-scale(x,center=TRUE, scale=FALSE)
+    .center<-attr(x,'scaled:center')
+  } else {
+    .center<-FALSE
+  }
+  if (is.logical(scale) && scale) {
+    x<-scale(x,center=FALSE,scale=TRUE)
+    .scale<-attr(x,'scaled:scale')
+  } else {
+    .scale<-FALSE
+  }
   n<-nrow(x)
   H<-diag(1,n)-matrix(1/n,n,n)
   L<-matrix(0,n,n)
@@ -21,13 +42,32 @@ xLabels ##<< classification of x (logical or factor)
     tmp<-xLabels==i
     L[tmp,tmp]<-1
   }
+  if (debug) {
+    .pn(x)
+    .pn(H)
+    .pn(L)
+  }
   Q<-t(x)%*%H%*%L%*%H%*%x
+  if (debug) {
+    .pn(Q)
+  }
   U<-eigen(Q)
+  if (debug) {
+    .pn(U)
+  }
   U$Q<-Q
+  if (retx) {
+    U$x<-x%*%U$vectors
+  }
+  U$center<-.center
+  U$scale<-.scale
   ### Eigenvalue decomposition of 'Q' (see the paper). The value is a
   ### list of 'values' and 'vectors' components (see 'base::eigen'),
-  ### plus 'Q', the matrix being decomposed. The number of eigen values
-  ### and eigenvectors correspond to the dimension of the output space.
+  ### 'Q', the matrix being decomposed, and 'center' and 'scale'
+  ### holding the centering and scaling used, or 'FALSE'.
+  ### If 'retx' is TRUE, the rotated version of 'x' is returned in 'x'.
+  ### The number of eigenvalues and eigenvectors correspond to the
+  ### dimension of the output space.
   return(U)
 },ex=function() {
   spca(iris[,1:4],iris$Species)
