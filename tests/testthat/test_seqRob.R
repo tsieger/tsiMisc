6d659aac38319f707bee4ef2e71d110b4077cb9f
Tomas Sieger <tsieger@users.noreply.github.com>
2016-03-10 11:27:31 +0100

    fixed

--- tests/testthat/test_seqRob.R
+++ tests/testthat/test_seqRob.R
@@ -1,5 +1,5 @@
-expect_that(seqRob(1,0,1), equals(1))
-expect_that(seqRob(0,1,-1), equals(0))
+expect_that(length(seqRob(1,0,1)), equals(0))
+expect_that(length(seqRob(0,1,-1)), equals(0))
 expect_that(seqRob(0,0,0), equals(0))
 
 expect_that(seqRob(0,1), equals(seq(0,1)))
