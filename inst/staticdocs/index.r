764a737f958b6efee38af3ea6d871627ce88da27
Tomas Sieger <tsieger@users.noreply.github.com>
2016-04-15 21:19:40 +0200

    staticdoc index added

---
+++ inst/staticdocs/index.r
@@ -0,0 +1,100 @@
+
+sd_section("Dev tools",
+  "Development tools",
+  c(
+    "reloadByName",
+    "buildInlineDoc",
+    "showObjectsSize"
+  )
+)
+
+sd_section("Graphics",
+  "Graphics, mostly 3D",
+  c(
+    "movieSpin3d",
+    "plot3dProj",
+    "plotImage",
+    "plotMatrix",
+    "rglSetMouseCbTrackball"
+  )
+)
+
+
+sd_section("Statistics",
+  "Statistical utils.",
+  c(
+    "VIF",
+    "compensateAlpha",
+    "compensatePValue",
+    "corCritVal",
+    "dagostinoTest",
+    "lmCalib",
+    "lmInt",
+    "lmIntXY",
+    "lmSignif",
+    "plot.lmInt",
+    "plot.lmIntXY",
+    "poissonTest",
+    "pvalStars",
+    "spca"
+  )
+)
+
+sd_section("Exploratory data analysis",
+  "EDA",
+  c(
+    "blandAltman",
+    "countNa",
+    "histv",
+    "normality2color",
+    "normality2flag",
+    "pairs",
+    "percentTrue",
+    "plotCor",
+    "plotShadow"
+  )
+)
+
+sd_section("Math tools",
+  "Mathematical tools",
+  c(
+    "cartesian2polar",
+    "cartesian2spherical",
+    "expit",
+    "lnormPar",
+    "logit",
+    "polar2cartesian",
+    "scaleToUnit",
+    "spherical2cartesian",
+    "vectorLength",
+    "vectorprod"
+  )
+)
+
+sd_section("Misc utils",
+  "Misc utils",
+  c(
+    "allSame",
+    "columnIndexOf",
+    "countFactorLevels",
+    "countIntersect",
+    "dropLevels",
+    "flip",
+    "iclass",
+    "indexOf",
+    "printWithName",
+    "refineFactors",
+    "repmat",
+    "seqRob",
+    "strTrim",
+    "to.matrix"
+  )
+)
+
+sd_section("Machine learning tools",
+  "ML tools",
+  c(
+    "perfInd"
+  )
+)
+
