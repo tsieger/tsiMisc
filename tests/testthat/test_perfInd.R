ab2ab30031ff2569dbbbe68a9834ff6f43d0cc89
Tomas Sieger <tsieger@users.noreply.github.com>
2016-04-23 16:31:50 +0200

    fix x,y arguments having no two distinct values

--- tests/testthat/test_perfInd.R
+++ tests/testthat/test_perfInd.R
@@ -17,3 +17,10 @@ expect_that(perfInd(c(0,0),c(0,1))$table, equals(m))
 
 m<-as.table(matrix(c(1,0,1,0),2,2,dimnames=list(x=c('0','1'),y=c('0','1'))))
 expect_that(perfInd(factor(c(0,0)),factor(c(0,1)))$table, equals(m))
+
+m<-as.table(matrix(c(0,0,0,0),2,2,dimnames=list(x=c('a','a2'),y=c('c','d'))))
+expect_that(perfInd(factor(c(0,0),levels=c('a')),factor(c(0,1),levels=c('c','d')))$table, equals(m))
+
+m<-as.table(matrix(c(1,0,1,0),2,2,dimnames=list(x=c('a','a2'),y=c('c','d'))))
+expect_that(perfInd(factor(c('a','a'),levels=c('a')),factor(c('c','d'),levels=c('c','d')))$table, equals(m))
+
