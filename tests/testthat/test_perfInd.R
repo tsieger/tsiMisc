96ba06ca04a1f049f41e0deedb414a8f3cc2ec4a
Tomas Sieger <tsieger@users.noreply.github.com>
2016-04-23 16:31:39 +0200

    fix x,y arguments having no two distinct values

--- tests/testthat/test_perfInd.R
+++ tests/testthat/test_perfInd.R
@@ -4,5 +4,16 @@ y<-rep(c(0,1),each=n)
 # expect no warning
 expect_silent(perfInd(x,y))
 
-m<-as.table(array(n/2,c(2,2),list(x=c(0,1),y=c(0,1))))
+m<-as.table(matrix(n/2,2,2,dimnames=list(x=c(0,1),y=c(0,1))))
 expect_that(perfInd(x,y)$table, equals(m))
+
+
+# check missing categories in x,y
+m<-as.table(matrix(c(1,0,0,0),2,2,dimnames=list(x=c('0','02'),y=c('0','02'))))
+expect_that(perfInd(0,0)$table, equals(m))
+
+m<-as.table(matrix(c(1,0,1,0),2,2,dimnames=list(x=c('0','1'),y=c('0','1'))))
+expect_that(perfInd(c(0,0),c(0,1))$table, equals(m))
+
+m<-as.table(matrix(c(1,0,1,0),2,2,dimnames=list(x=c('0','1'),y=c('0','1'))))
+expect_that(perfInd(factor(c(0,0)),factor(c(0,1)))$table, equals(m))
