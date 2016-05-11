41da15d2e717b4710d0ef60e38896c6f6bb0f536
Tomas Sieger <tsieger@users.noreply.github.com>
2016-05-11 16:33:40 +0200

    rowMatrix argument added

--- R/to.matrix.R
+++ R/to.matrix.R
@@ -3,21 +3,30 @@ function # Conversion to a matrix.
 ##description<<
 ## 'to.matrix' attempts to convert any data to a row matrix. In
 ## particular, data frames are converted by 'as.matrix', (numeric)
-## vectors are converted to a single row matrix.
-(x ##<< data to convert to a matrix
+## vectors are converted to a single row/column matrix, according to
+## the \code{rowMatrix} argument.
+(x, ##<< data to convert to a matrix
+rowMatrix = TRUE ##<< if \code{TRUE}, numeric vectors are converted to
+## a single row matrix, if \code{FALSE}, they get converted to a single
+## column matrix
 )
 {
   if (!is.matrix(x)) {
     if (is.data.frame(x)) {
       x<-as.matrix(x)
     } else {
-      x<-matrix(x,nrow=1)
+      if (rowMatrix) {
+        x<-matrix(x,nrow=1)
+      } else {
+        x<-matrix(x,ncol=1)
+      }
     }
   }
   return(x)
   ### matrix
 },ex=function() {
   to.matrix(1:3)
+  to.matrix(1:3,rowMatrix=FALSE)
   to.matrix(iris[1:3,1:4])
   to.matrix(matrix(1:4,2))
 })
