fbd0e187419dde09e4f735fdb7dcad9409fee1b3
Tomas Sieger <tsieger@users.noreply.github.com>
2016-06-14 14:11:11 +0200

    fixing a bug, simplifying

--- R/computeDistIdx.R
+++ R/computeDistIdx.R
@@ -9,29 +9,20 @@ i ##<< index of point to compute indices for
 ) {
   if (i>n) stop('i must be less or equal to n')
   if (i==1) {
-    i1<-c()
-    i2<-c()
+    idx<-1:(n-1)
   } else {
-    i1<-i-1+cumsum(c(0,seq(n-2,n-2-(i-3),-1)))
+    # from 1 ... i-1 to i
+    idx<-i-1+cumsum(c(0,seq(n-2,n-2-(i-3),-1)))
+    # from i to i+1 ... n
     tmp<-1+(2*n-i)*(i-1)/2
-    i2<-tmp:(tmp+n-i-1)
+    i2<-c(idx,seqRob(tmp,tmp+n-i-1,1))
   }
-  i<-c(i1,i2)
-  attr(i,'i1')<-i1
-  attr(i,'i2')<-i2
-  return(i)
+  return(idx)
   ### a vector of indices of entries in the distance matrix related to
   ### the distances from the \code{i}-th point to the other points.
   ### The indices of entries corresponding to the distance between
-  ### point \code{j < i} and \code{i} get also returned in the
-  ### \code{i1} attribute of the return value, and the indices of
-  ### entries corresponding to the distances between point \code{j > i}
-  ### and \code{i} get returned in the \code{i2} attribute of the
-  ### return value. These attributes can be useful to construct a
-  ### vector of length \code{n} corresponding to each of the individual
-  ### observations distance matrix is computed from, by concatenating
-  ### some measure based on \code{i1}, some value relevant to the
-  ### point \code{i} itself, and some measure based on \code{i2}.
+  ### point \code{j} and point \code{i} are sorted in increasing order
+  ### according to \code{j}.
 },ex=function() {
   x<-1:10
   d<-dist(x)
@@ -40,11 +31,4 @@ i ##<< index of point to compute indices for
   # other points
   i<-computeDistIdx(length(x),3)
   print(i)
-
-  # entries holding distances from 1st and 2nd points to the 3rd point:
-  print(attr(i,'i1'))
-  # entries holding distances from the 3rd point to the 4th, ..., 10th
-  # points:
-  print(attr(i,'i2'))
-
 })
