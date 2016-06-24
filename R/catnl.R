34209231b90361dad1b980ad070663b01c782468
Tomas Sieger <tsieger@users.noreply.github.com>
2016-06-24 17:07:21 +0200

    'catnl' added

---
+++ R/catnl.R
@@ -0,0 +1,11 @@
+catnl<-structure(
+function # Cat with trailing newline.
+##description<<
+## \code{cat} with trailing newline.
+(... ##<< arguments to cat
+) {
+  cat(...,'\n')
+  ### None.
+},ex=function() {
+  catnl('Hello, world!')
+})
