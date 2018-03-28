3d19bbb250362998905c5eab2a2605aad33ccd58
Tomas Sieger <tsieger@users.noreply.github.com>
2018-03-28 21:25:44 +0200

    3d plot demos wrapped in 'if (interactive() && require(rgl))'

--- R/txPca.R
+++ R/txPca.R
@@ -59,8 +59,10 @@ k = 3, ##<< number of dimensions of the result, defaults to 3 in order
   tx<-txPca(iris[,1:4])
   plot(tx(iris[,1:4])[,1:2],pch=19,col=c('red','green','blue')[as.numeric(iris$Species)])
 
-  # a 3D example
-  x<-iris[,1:4]
-  y<-iris$Species
-  plot3dProj(x, cls=y, tx=txPca(x))
+  if (interactive() && require(rgl)) {
+    # a 3D example
+    x<-iris[,1:4]
+    y<-iris$Species
+    plot3dProj(x, cls=y, tx=txPca(x))
+  }
 })
--- R/txSpca.R
+++ R/txSpca.R
@@ -73,8 +73,10 @@ k = 3, ##<< number of dimensions of the result, defaults to 3 in order
   plot(txPca(iris[,1:4])(iris[,1:4])[,1:2],col=c('red','green','blue')[as.numeric(iris$Species)])
   par(opar)
 
-  # a 3D example
-  x<-iris[,1:4]
-  y<-iris$Species
-  plot3dProj(x, cls=y, tx=txSpca(x,y))
+  if (interactive() && require(rgl)) {
+    # a 3D example
+    x<-iris[,1:4]
+    y<-iris$Species
+    plot3dProj(x, cls=y, tx=txSpca(x,y))
+  }
 })
