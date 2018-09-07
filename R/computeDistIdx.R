c1d3e25d4490031f81780bd5157a4f2a10907ff8
Tomas Sieger <tsieger@users.noreply.github.com>
2018-09-07 11:22:44 +0200

    eanbled to compute indices to more than a single point

--- R/computeDistIdx.R
+++ R/computeDistIdx.R
@@ -3,10 +3,14 @@ function # Indices into distance matrix.
 ##description<<
 ## \code{\link{computeDistIdx}} computes the indices of entries in a
 ## distance matrix (as computed by \code{\link[stats]{dist}}) that hold
-## the distances between a given point and all the other points.
+## the distances between a given point and one or more other points.
+##seealso<<
+## computeIdxFromDistIdx
 (n, ##<< number of points
-i, ##<< index of point to compute indices for
-includeMyself = FALSE ##<< if TRUE, the returned vector includes an
+i, ##<< index of point to compute distance from
+j = setdiff(1:n, i), ##<< index of point(s) to compute distance to
+includeMyself = FALSE ##<< if TRUE, and \code{j} equals
+## \code{setdiff(1:n, i)}, the returned vector includes an
 ## extra \code{NA} element corresponding to the non-existing index into
 ## the distance matrix holding the distance between point \code{i} and
 ## point \code{i} itself. This makes the length of the returned vector
@@ -14,9 +18,28 @@ includeMyself = FALSE ##<< if TRUE, the returned vector includes an
 ## distance between point \code{i} and point \code{j} resides at
 ## position \code{j} in the returned vector.
 ) {
-  if (i>n) stop('i must be less or equal to n')
+  if (length(i)!=1) stop('`i\' must be of length 1')
+  if (i>n) stop('`i\' must be less or equal to `n\'')
+  if (any(j>n)) stop('`j\' must be less or equal to `n\'')
+  if (i%in%j) stop('`j\' must not contain `i\'')
+
+  if (includeMyself && !(length(j)==n-1 && all(j==setdiff(1:n,i)))) {
+    warning('`includeMyself\' set, but `j\' is not `setdiff(1:n, i)\', disabling `includeMyself\'')
+    includeMyself<-FALSE
+  }
+  i1<-j[j<i]
+  idx1<-n*(i1-1) - i1*(i1-1)/2 + i-i1
+  j2<-j[j>i]
+  idx2<-n*(i-1) - i*(i-1)/2 + j2-i
+  if (includeMyself) {
+    idx<-c(idx1,NA,idx2)
+  } else {
+    idx<-c(idx1,idx2)
+  }
+
+  if (0) { # old implementation
   if (i==1) {
-    idx<-1:(n-1)
+    idx<-j-1
     if (includeMyself) idx<-c(NA,idx)
   } else {
     # from 1 ... i-1 to i
@@ -26,6 +49,8 @@ includeMyself = FALSE ##<< if TRUE, the returned vector includes an
     tmp<-1+(2*n-i)*(i-1)/2
     idx<-c(idx,rseq(tmp,tmp+n-i-1,1))
   }
+  }
+
   return(idx)
   ### a vector of indices of entries in the distance matrix related to
   ### the distances from the \code{i}-th point to the other points.
@@ -51,5 +76,9 @@ includeMyself = FALSE ##<< if TRUE, the returned vector includes an
   i <- computeDistIdx(length(x), 3, includeMyself = TRUE)
   i
   # the distance between point 3 and 4:
-  i[4]
+  d[i[4]]
+
+  # entries holding distances between the 3rd point and points 5 and 7
+  computeDistIdx(length(x), 3, c(5, 7))
+
 })
