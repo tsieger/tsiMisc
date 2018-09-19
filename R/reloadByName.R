8152f9f80fa391edf075f2c477867adee6b4f62a
Tomas Sieger <tsieger@users.noreply.github.com>
2018-09-19 16:20:58 +0200

    unloading/loading in LIFO order

--- R/reloadByName.R
+++ R/reloadByName.R
@@ -9,16 +9,20 @@ function # Reload R package(s).
   # attempt to accept also non-quoted package names
   nm<-deparse(substitute(pkgName))
   # a hack - if there is no quoted quote, the argument is not a
-  # character string (not we can;t simply use `is.character()' as this
+  # character string (not we can't simply use `is.character()' as this
   # would have evaluated the argument which is NOT a name of a
   # variable)
   if (regexpr('\"',nm)==-1) {
     # use the deparsed argument as the package
     pkgName<-nm
   }
-
+  
   for (x in pkgName) {
+    catnl(paste0('unloading ',x))
     devtools::unload(devtools::inst(x))
+  }
+  for (x in rev(pkgName)) {
+    catnl(paste0('loading ',x))
     require(x,character.only=TRUE)
   }
 },ex=function() {
