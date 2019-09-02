6ebf4a43ce4d77211d9a390dffc7e40320e7c82a
tsieger <tomas@sieger>
2019-09-02 13:10:29 +0200

    a bug fixed

--- R/rmLcPostfix.R
+++ R/rmLcPostfix.R
@@ -13,6 +13,7 @@ function # Remove longest common postfix.
 (x, ##<< a vector of character strings
 ignore.case = FALSE ##<<
 ) {
+  x<-as.character(x)
   p<-lcPostfix(x,ignore.case=ignore.case)
   n<-nchar(p)
   for (i in seq(along=x)) {
