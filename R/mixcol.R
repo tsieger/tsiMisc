8b55b4e000b85e137bb4c220087c3210611647a7
tsieger <tomas@sieger>
2019-09-26 17:08:48 +0200

    mixcol implemented

---
+++ R/mixcol.R
@@ -0,0 +1,41 @@
+mixcol <- structure(
+function # Mix colors in textual representation.
+##description<<
+##
+## Mix (blend) two colors specified as text. This is high-level function
+## based on the \code{\link[colorspace]{mixcolor}}.
+##
+##seealso<< \code{\link[colorspace]{mixcolor}},
+## \code{\link[grDevices]{col2rgb}},
+## \code{\link[colorspace]{RGB}},
+## \code{\link{rgb2hex}}.
+(col1, ##<< the first color (a character vector or an integer color code)
+col2, ##<< the second color (a character vector or an integer color code)
+r = 0.5 ##<< amount of the first color (a number between 0 and 1)
+){
+  if (!is.numeric(r) || length(r)!=1 || r<0 || r>1) {
+    stop('invalid "r" argument')
+  }
+  if (!is.character(col1) && !is.integer(col1)) {
+    stop('invalid "col1" color specification, expected a character vector or an integer')
+  }
+  if (!is.character(col2) && !is.integer(col2)) {
+    stop('invalid "col2" color specification, expected a character vector or an integer')
+  }
+
+  col1<-RGB(t(col2rgb(col1)))
+  col2<-RGB(t(col2rgb(col2)))
+  mc<-mixcolor(r,col1,col2)
+  col<-rgb2hex(mc)
+
+  return(col)
+  ### textual representation of the mixed color
+},ex=function() {
+  col1<-mixcol('yellow','black',0)
+  col2<-mixcol('yellow','black', .25)
+  col3<-mixcol('yellow','black', .5)
+  col4<-mixcol('yellow','black', .75)
+  col5<-mixcol('yellow','black',1)
+  plot(0:4,rep(0,5),xlim=c(-.5,5.5),ty='p',pch=21,cex=15,col='black',
+    bg=c(col1,col2,col3,col4,col5),frame=FALSE,xlab='',ylab='',xaxt='n',yaxt='n')
+})
---
+++ tests/testthat/test_mixcol.R
@@ -0,0 +1,12 @@
+expect_that(mixcol('black','white'), equals('#7F7F7F'))
+expect_that(mixcol('black','white',.5), equals('#7F7F7F'))
+expect_that(mixcol('black','white',0), equals('#000000'))
+expect_that(mixcol('black','white',1), equals('#FFFFFF'))
+
+expect_error(mixcol())
+expect_error(mixcol(NULL))
+expect_error(mixcol(NULL,NULL))
+expect_error(mixcol('a','b'))
+expect_error(mixcol('red','red',-1))
+expect_error(mixcol('red','red',NULL))
+expect_error(mixcol('red','red','red'))
