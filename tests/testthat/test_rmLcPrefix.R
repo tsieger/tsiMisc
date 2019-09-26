e291f95de6c819e9309fb8a72e344ff653b75987
tsieger <tomas@sieger>
2019-09-26 17:14:58 +0200

    fixed

--- tests/testthat/test_rmLcPrefix.R
+++ tests/testthat/test_rmLcPrefix.R
@@ -1,9 +1,7 @@
 expect_that(rmLcPrefix(c('abcd','abef','aqwe')), equals(c('bcd','bef','qwe')))
 expect_that(rmLcPrefix(c('abcd','abef','abqwe')), equals(c('cd','ef','qwe')))
 expect_that(rmLcPrefix(c('abc','abc')), equals(c('','')))
-expect_that(rmLcPrefix(c('abc')), equals(''))
+expect_that(rmLcPrefix(c('abc')), equals('abc'))
 expect_that(rmLcPrefix(''), equals(''))
-expect_that(rmLcPrefix(NULL), equals(NULL))
-
+expect_that(rmLcPrefix(NULL), equals(character(0)))
 expect_that(rmLcPrefix(factor(c('abc','abd'))), equals(c('c','d')))
-
