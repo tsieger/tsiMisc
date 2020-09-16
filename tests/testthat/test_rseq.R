3940b6b48e9f55bbe9ca02548cec316aa8aa2e8f
tsieger <tomas@sieger>
2020-09-16 14:11:30 +0200

    up

--- tests/testthat/test_rseq.R
+++ tests/testthat/test_rseq.R
@@ -1,8 +1,14 @@
-expect_that(length(rseq(1,0,1)), equals(0))
-expect_that(length(rseq(0,1,-1)), equals(0))
-expect_that(rseq(0,0,0), equals(0))
+context('rseq')
 
-expect_that(rseq(0,1), equals(seq(0,1)))
-expect_that(rseq(0,1,1), equals(seq(0,1)))
-expect_that(rseq(1,0), equals(seq(1,0)))
-expect_that(rseq(1,0,-1), equals(seq(1,0)))
+test_that("empty rseq obtained", {
+    expect_that(length(rseq(1,0,1)), equals(0))
+    expect_that(length(rseq(0,1,-1)), equals(0))
+    expect_that(rseq(0,0,0), equals(0))
+})
+
+test_that("valid rseq obtained", {
+    expect_that(rseq(0,1), equals(seq(0,1)))
+    expect_that(rseq(0,1,1), equals(seq(0,1)))
+    expect_that(rseq(1,0), equals(seq(1,0)))
+    expect_that(rseq(1,0,-1), equals(seq(1,0)))
+})
