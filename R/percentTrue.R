3d5cfbf72a60d1484b1f9914aaa0e880b7646e35
tsieger <tomas@sieger>
2021-10-05 17:00:48 +0200

    na.rm arg added

--- R/percentTrue.R
+++ R/percentTrue.R
@@ -4,12 +4,13 @@ function # Percentual ratio of TRUE cases.
 ## \code{\link{percentTrue}} computes the percentual ratio of
 ## \code{TRUE} cases
 (x, ##<< a logical vector (or a vector that can be coerced to one)
-digits = NULL ##<< number of decimal places (passed to \code{round})
+digits = NULL, ##<< number of decimal places (passed to \code{round})
+... ##<< additional arguments passed to \code{sum()}
 ) {
   if (!is.logical(x)) {
     x<-as.logical(x)
   }
-  rv<-100*sum(x)/length(x)
+  rv<-100*sum(x,...)/length(x)
   if (!is.null(digits)) {
     rv<-round(rv,digits=digits)
   }
