2374ca74388c7d73e31e85d5c8e21be9d8e2b950
tsieger <tomas@sieger>
2021-11-09 10:13:45 +0100

    cpcol added

--- R/cpcol.R
+++ R/cpcol.R
@@ -1,12 +1,12 @@
-rencol<-structure(
-function # Rename columns in a data frame.
+cpcol<-structure(
+function # Copy columns in a data frame.
 ##description<<
-## \code{\link{rencol}} renames columns in a data frame.
+## \code{\link{cpcol}} copies columns in a data frame.
 ##
-##seealso<< cpcol
+##seealso<< rencol
 (d, ##<< data frame
-on, ##<< original column name (or a regexpr)
-nn, ##<< desired column name (or a regexpr)
+on, ##<< original column name (or part of it, or a regexpr)
+nn, ##<< desired column name (or part of it, or a regexpr)
 i = NULL, ##<< optional column index that must match the column with the name
 ## denoted by \code{on} (this serves as a sanity check ensuring the
 ## change affects the intended column)
@@ -19,15 +19,24 @@ multi = FALSE ##<< logical flag indicating whether to change multiple
 
   if (!regexp) {
     on<-gsub('\\(','\\\\(',on)
+    nn<-gsub('\\(','\\\\(',nn)
     on<-gsub('\\)','\\\\)',on)
+    nn<-gsub('\\)','\\\\)',nn)
     on<-gsub('\\.','\\\\.',on)
+    nn<-gsub('\\.','\\\\.',nn)
   }
 
   if (multi) {
-    if (length(grep(paste0(on,'.*'),colnames(d)))==0) {
+    idx<-grep(paste0(on,'.*'),colnames(d))
+    if (length(idx)==0) {
       stop('no columns match the pattern "',on,'"')
     }
-    colnames(d)<-gsub(on,nn,colnames(d))
+    for (ii in idx) {
+      d<-cbind(d,d[,ii],stringsAsFactors=FALSE)
+    }
+    cn<-colnames(d)
+    cn[(ncol(d)-length(idx)+1):ncol(d)]<-gsub(on,nn,cn[idx])
+    colnames(d)<-cn
   } else {
     if (!is.null(i)) {
       if (!is.numeric(i) || length(i)!=1 || i>length(colnames(d))) {
@@ -36,7 +45,8 @@ multi = FALSE ##<< logical flag indicating whether to change multiple
       if (grep(on,colnames(d)[i])!=1) {
         stop(paste0('colname at index ',i,': "',colnames(d)[i],'" does not match "',on,'"'))
       }
-      colnames(d)[i]<-nn
+      d<-cbind(d,d[,i],stringsAsFactors=FALSE)
+      colnames(d)[ncol(d)]<-nn
     } else {
       ii<-grep(on,colnames(d))
       if (length(ii)>1) {
@@ -45,16 +55,18 @@ multi = FALSE ##<< logical flag indicating whether to change multiple
       if (length(ii)==0) {
         stop(paste0('no column matches "',on,'"'))
       }
-      colnames(d)[ii]<-nn
+      d<-cbind(d,d[,ii],stringsAsFactors=FALSE)
+      colnames(d)[ncol(d)]<-nn
     }
   }
   return(d)
 },ex=function() {
   tmp<-data.frame(a=1:2,b=2:3)
-  rencol(tmp,'a','a2')
-  rencol(tmp,'a','a2',1)
+  cpcol(tmp,'a','a2')
+  # copy with explicit column idx check
+  cpcol(tmp,'a','a2',1)
 
-  # get rid of a common prefix
-  tmp<-data.frame(prefix.a=1:2,prefix.b=2:3)
-  rencol(tmp,'prefix.','',multi=TRUE)
+  # copy all columns starting with 'a.' as 'b.'
+  tmp<-data.frame(a.x=1:2,a.b=2:3)
+  cpcol(tmp,'a.','b.',multi=TRUE)
 })
